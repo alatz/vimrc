ab3088870363aa19b2c1c3545d8f3edea3954a83
alatz <aaron@aaronlatz.com>
2016-05-17 08:29:22 -0700

    Add fish and spacemacs configs

---
+++ config.fish
@@ -0,0 +1,24 @@
+#fishrc
+
+#git aliases
+alias gs='git status '
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit'
+alias gd='git diff'
+alias go='git checkout '
+alias gk='gitk --all&'
+alias gx='gitx --all'
+alias got='git '
+alias get='git '
+alias gpom='git push origin master'
+
+#set default editor
+set -x EDITOR vim
+
+set -g -x PATH
+set -g -x PATH $PATH /Applications/Racket\ v6.4/bin
+set -g -x PATH $PATH /usr/local/Cellar/emacs-mac/emacs-24.5-z-mac-5.17/bin/
+set -g -x PATH $PATH /usr/local/bin
+set -g -x PATH $PATH /usr/bin /bin /usr/sbin /sbin /Users/aaronl/.local/bin
+
